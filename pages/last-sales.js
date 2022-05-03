518591a54411e8ede22f1ec4b40f3dc6f90f47a8
E-K8 <ekatplat@gmail.com>
2022-05-03 23:29:39 +0100

    practice client-side fetching, install and implement useSWR, still  errors

---
+++ pages/last-sales.js
@@ -0,0 +1,68 @@
+import { useEffect, useState } from 'react';
+import useSWR from 'swr';
+
+function LastSalesPage() {
+  const [sales, setSales] = useState();
+  // const [isLoading, setIsLoading] = useState(false);
+
+  const { data, error } = useSWR(
+    'https://nextjs-data-fetch-ebf5b-default-rtdb.europe-west1.firebasedatabase.app/sales.json'
+  );
+
+  useEffect(() => {
+    if (data) {
+      const transformedSales = [];
+
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
+
+  // useEffect(() => {
+  //   setIsLoading(true);
+
+  //   fetch(
+  //     'https://nextjs-data-fetch-ebf5b-default-rtdb.europe-west1.firebasedatabase.app/sales.json'
+  //   )
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const transformedSales = [];
+  //       for (const key in data) {
+  //         transformedSales.push({
+  //           id: key,
+  //           username: data[key].username,
+  //           volume: data[key].volume,
+  //         });
+  //       }
+
+  //       setSales(transformedSales);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+
+  if (error) {
+    return <p>Failed to load</p>;
+  }
+
+  if (!data || !sales) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          {sale.username} - £{sale.volume}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default LastSalesPage;
